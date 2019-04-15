b0538dc0e532576d38f81e260d18ca055364d762
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-15 16:29:30 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++){
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+function whileLoop(n) {
+  while (n > 0){
+    console.log(100)
+      n--
+  }
+  return 'done'
+}
+function doWhileLoop(num) {
+  function incrementVariable() {
+   let i = i + 1; i === 0; i < 10;
+  }
+  do {
+   console.log("I run once regardless.");
+  } while (incrementVariable() < num)
+  return num
+}
